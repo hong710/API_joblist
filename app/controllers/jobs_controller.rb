67e644e7f01eaf594949941d3a92b00950589fd9
Anthony Hong <tony.hongq@gmail.com>
2022-07-02 20:18:03 -0700

    add post API to add job

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,7 +1,19 @@
 class JobsController < ApplicationController
+    wrap_parameters format: []
     def index
         jobs = Job.all
         render json: jobs, status: :ok
+    end
+
+    def create
+        job = Job.create(job_params)
+        render json: job, status: :created        
+    end
+
+    private
+
+    def job_params
+        params.permit(:client, :poc, :poc_email, :urgency, :role, :quantity, :skills)
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources :jobs, only: [:index, :show]
+  resources :jobs, only: [:index, :show, :create]
 end
